4d6f5b95322344b41c952f6f1d51b275c36730ec
Piperaptor <55030769+Piperaptor@users.noreply.github.com>
2022-07-05 23:38:49 -0400

    Update CalcularnuevospreciosApplicationTests.java

--- src/test/java/com/metodos/calcularnuevosprecios/CalcularnuevospreciosApplicationTests.java
+++ src/test/java/com/metodos/calcularnuevosprecios/CalcularnuevospreciosApplicationTests.java
@@ -1,9 +1,9 @@
 package com.metodos.calcularnuevosprecios;
 
 import org.junit.jupiter.api.Test;
---import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
---@SpringBootTest
+@SpringBootTest
 class CalcularnuevospreciosApplicationTests {
 
 	//@Test
